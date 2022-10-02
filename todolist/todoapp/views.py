af1dd96ab0b08e3f90d6a8ecc4971644239b0a04
jungwook02 <aking0520@naver.com>
2022-10-02 13:24:15 +0900

    todolist세션

---
+++ todolist/todoapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .serializers import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+@api_view(["GET"])
+def todolist(req):
+    todos = Todo.objects.all()
+    serializer = TodoSerializer(todos, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"]) 
+def todocreate(req):
+    serializer = TodoSerializer(data=req.data)
+    
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.error)
+
+@api_view(["PUT"])
+def todoupdate(req,pk):
+    todo = Todo.objects.get(id=pk)
+    serializer = TodoSerializer(todo, data=req.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.error)
+
+@api_view(["DELETE"])
+def tododelete(req,pk):
+
+    todo = Todo.object.get(id=pk)
+    todo.delete()
+    return Response("삭제되었습니다.")
+
